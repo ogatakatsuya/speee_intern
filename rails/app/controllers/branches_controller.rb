ae0d2a18670f0d17a8be5a516b7df6c6224f95b4
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-08-23 07:05:21 +0000

    #13 controllerのクエリを1つに変更

--- rails/app/controllers/branches_controller.rb
+++ rails/app/controllers/branches_controller.rb
@@ -7,9 +7,5 @@ class BranchesController < ApplicationController
 
   def show
     @branch = Branch.find(params[:id])
-    @company = Company.find(@branch.company_id)
-    @branch_city = City.find(@branch.city_id)
-    @branch_prefecture = Prefecture.find(@branch_city.prefecture_id)
-    @reviews = @branch.reviews
   end
 end
