5a080f7c481e4c85f028b0c4ea0d6a43a0d8e294
shunsei <72291064+kimurash@users.noreply.github.com>
2024-08-26 05:55:52 +0000

    テストを一旦削除

--- rails/test/controllers/assessment_requests_controller_test.rb
+++ rails/test/controllers/assessment_requests_controller_test.rb
@@ -1,15 +1,1 @@
-# frozen_string_literal: true
 
-require 'test_helper'
-
-class AssessmentRequestsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get new' do
-    get assessment_requests_new_url
-    assert_response :success
-  end
-
-  test 'should get create' do
-    get assessment_requests_create_url
-    assert_response :success
-  end
-end
