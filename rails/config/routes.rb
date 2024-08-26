b8da26bdd8c68128d9427fee6c633ef56227bf7d
shunsei <72291064+kimurash@users.noreply.github.com>
2024-08-26 04:58:33 +0000

    余計なルーティングを削除した

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'assessment_requests/new'
-  get 'assessment_requests/create'
   mount KomachiHeartbeat::Engine => '/ops'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
