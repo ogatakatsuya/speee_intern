abc845723f4e4c4dcce2acc1964efbd2c2cce860
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-08-28 05:34:54 +0000

    resultのリファクタリングとパラメータの検証を追加

--- rails/app/controllers/branches_controller.rb
+++ rails/app/controllers/branches_controller.rb
@@ -20,20 +20,52 @@ class BranchesController < ApplicationController
   end
 
   def result
+    load_dependencies
+
+    redirect_to branches_path and return if invalid_prefecture_id? || invalid_city_id?
+
+    if filtering_by_prefecture?
+      filter_branches_by_prefecture
+    else
+      filter_branches_by_city
+    end
+  end
+
+  private
+
+  def load_dependencies
     @prefectures = Prefecture.all
     @cities = City.all
     @area_filtering_form = AreaFilteringForm.new
     @branches = Branch.preload({ city: :prefecture }, :company, reviews: { city: :prefecture })
+  end
 
-    if params[:city_id].present? && params[:city_id].to_i.zero?
-      @branches = @branches.joins(:city).where(cities: { prefecture_id: params[:prefecture_id] })
-      @branches_per_page = @branches.paginate(page: params[:page], per_page: 10)
-      @branch_address = Prefecture.find_by(id: params[:prefecture_id]).name
-    else
-      @branches = @branches.where(city_id: params[:city_id]) if params[:city_id].present?
-      @branches_per_page = @branches.paginate(page: params[:page], per_page: 10)
-      @city = City.find_by(id: params[:city_id])
-      @branch_address = @city.city_address if @city.present?
-    end
+  def invalid_prefecture_id?
+    params[:prefecture_id].present? && !numeric?(params[:prefecture_id])
+  end
+
+  def invalid_city_id?
+    params[:city_id].present? && !numeric?(params[:city_id])
+  end
+
+  def filtering_by_prefecture?
+    params[:city_id].present? && params[:city_id].to_i.zero?
+  end
+
+  def filter_branches_by_prefecture
+    @branches = @branches.joins(:city).where(cities: { prefecture_id: params[:prefecture_id] })
+    @branches_per_page = @branches.paginate(page: params[:page], per_page: 10)
+    @branch_address = Prefecture.find_by(id: params[:prefecture_id])&.name
+  end
+
+  def filter_branches_by_city
+    @branches = @branches.where(city_id: params[:city_id]) if params[:city_id].present?
+    @branches_per_page = @branches.paginate(page: params[:page], per_page: 10)
+    @city = City.find_by(id: params[:city_id])
+    @branch_address = @city.city_address if @city.present?
+  end
+
+  def numeric?(string)
+    string.match?(/\A-?\d+(\.\d+)?\z/)
   end
 end
