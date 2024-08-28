44abf431f2ff39326618213f20b74afa153b2b9b
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-08-28 08:50:33 +0000

    #46 jsの発火条件をレーダーチャートだけに限定

--- rails/app/javascript/application.js
+++ rails/app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-window.addEventListener('DOMContentLoaded turbo:load', () => {
-    import "@hotwired/turbo-rails"
-    import "controllers"
-}
+import "@hotwired/turbo-rails"
+import "controllers"
