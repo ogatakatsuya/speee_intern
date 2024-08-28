ad94f45a63f8b783095a4198f4f3d1adbc8323b6
wakij <73282956+wakij@users.noreply.github.com>
2024-08-28 05:12:52 +0000

    normalizerクラスを作成してリファクタリング

--- rails/app/controllers/branches_controller.rb
+++ rails/app/controllers/branches_controller.rb
@@ -6,36 +6,10 @@ class BranchesController < ApplicationController
   end
 
   def show
-    @branch = Branch.preload(:company, { city: :prefecture }, reviews: { city: :prefecture }).find(params[:id])
-
-    # レビューに関連する全体のデータを一度に取得(db依存性がある MySQL用)
-    branch_reviews = @branch.reviews.select(
-      'AVG(company_satisfaction_id) AS average_satisfaction',
-      'AVG(contract_price - appraisal_price) AS min_sales_satisfaction_indicator',
-      'AVG(DATEDIFF(sale_period, listing_period)) AS min_sales_speed_indicator'
-    ).first
-
-    @average_responsiveness_satisfaction = branch_reviews.average_satisfaction
-
-    # 売却満足度の指標の正規化
-    min_sales_satisfaction = Review.minimum('contract_price - appraisal_price')
-    max_sales_satisfaction = Review.maximum('contract_price - appraisal_price')
-    # 差があればあるほど値が小さくなるように反転させている
-    sales_satisfaction_range = max_sales_satisfaction - min_sales_satisfaction
-    scaled_sales_satisfaction = (((branch_reviews.min_sales_satisfaction_indicator - min_sales_satisfaction) * 4) / sales_satisfaction_range) # rubocop:disable Layout/LineLength
-    @normalized_sales_satisfaction = 6 - (1 + scaled_sales_satisfaction)
-
-    # 売却速度に関する指標の正規化
-    sales_speed_limits = Review.select(
-      "MIN(DATEDIFF(sale_period, listing_period)) AS min_sales_speed,
-      MAX(DATEDIFF(sale_period, listing_period)) AS max_sales_speed"
-    ).first
-
-    min_sales_speed = sales_speed_limits.min_sales_speed
-    max_sales_speed = sales_speed_limits.max_sales_speed
-    # 差があればあるほど値が小さくなるように反転させている
-    sales_speed_range = max_sales_speed - min_sales_speed
-    scaled_sales_speed = (((branch_reviews.min_sales_speed_indicator - min_sales_speed) * 4) / sales_speed_range)
-    @normalized_sales_speed = 6 - (1 + scaled_sales_speed)
+    @branch = Branch.preload(:company, city: :prefecture, reviews: { city: :prefecture }).find(params[:id])
+    evaluate_value = @branch.evaluate_value
+    @average_responsiveness_satisfaction = evaluate_value[:average_responsiveness_satisfaction]
+    @normalized_sales_satisfaction = evaluate_value[:normalized_sales_satisfaction]
+    @normalized_sales_speed = evaluate_value[:normalized_sales_speed]
   end
 end
