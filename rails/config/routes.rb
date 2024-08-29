5829460138b7bdbd7b9e20095e9219bc7b62dad7
shunsei <72291064+kimurash@users.noreply.github.com>
2024-08-29 03:29:24 +0000

    #59 パンくずリストを作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'branches#index'
+
   resources :branches, only: %i[index show] do
     collection do
       get :result
